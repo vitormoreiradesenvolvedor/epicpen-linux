98ae8dca8af7231e5840b006352d0c8f5dd479c3
Vitor Moreira <vitormoreiradesenvolvedor@gmail.com>
2026-07-28 13:59:52 -0300

    chore: bump versão para 1.0.18

--- src/main.py
+++ src/main.py
@@ -69,7 +69,7 @@ import layershell
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("EpicPen")
-    app.setApplicationVersion("1.0.17")
+    app.setApplicationVersion("1.0.18")
     app.setQuitOnLastWindowClosed(False)
 
     settings = cfg.load()
